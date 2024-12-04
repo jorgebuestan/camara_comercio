05b03d2738f8b81ac5226c88f061e8e45dfac86d
Andres Toala <ai_toala@outlook.com>
2024-12-04 17:34:07 -0500

    Creacion de modelos Socio, TipoIdentificacion, TIpoPersona y TipoPersoneria

--- app/Models/Socio.php
+++ app/Models/Socio.php
@@ -2,10 +2,52 @@
 
 namespace App\Models;
 
+use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\TipoIdentificacion;
+use App\Models\TipoPersona;
+use App\Models\TipoPersoneria;
 
 class Socio extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+    protected $table = 'socios';
+    protected $fillable = [
+        'logo',
+        'numero_consecutivo',
+        'fecha_ingreso',
+        'id_tipo_persona',
+        'id_tipo_personeria',
+        'id_tipo_identificacion',
+        'identificacion',
+        'razon_social',
+        'correo',
+        'telefono',
+        'cedula_representante_legal',
+        'nombres_representante_legal',
+        'apellidos_representante_legal',
+        'telefono_representante_legal',
+        'correo_representante_legal',
+        'fecha_registro_mercantil',
+        'fecha_vencimiento_nombramiento',
+        'fecha_desafiliacion',
+        'motivo_desafiliacion',
+        'estado'
+    ];
+
+    public function tipo_identificacion()
+    {
+        return $this->hasOne(TipoIdentificacion::class, 'id', 'id_tipo_identificacion');
+    }
+
+    public function tipo_persona()
+    {
+        return $this->hasOne(TipoPersona::class, 'id', 'id_tipo_persona');
+    }
+
+    public function tipo_personeria()
+    {
+        return $this->hasOne(TipoPersoneria::class, 'id', 'id_tipo_personeria');
+    }
 }
--- app/Models/TipoPersoneria.php
+++ app/Models/TipoPersoneria.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TipoPersoneria extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+    protected $table = 'tipo_personeria';
+    protected $fillable = [
+        'descripcion'
+    ];
 }
