a45b29f76446015e16d686526637cb71f1afaf40
Andres Toala <ai_toala@outlook.com>
2024-12-05 17:20:41 -0500

    feat: Enhance SocioController to include TipoPersona, TipoPersoneria, and TipoIdentificacion data in responses

--- app/Http/Controllers/SocioController.php
+++ app/Http/Controllers/SocioController.php
@@ -8,8 +8,12 @@ use App\Models\Provincia;
 use App\Models\Canton;
 use App\Models\Parroquia;
 use App\Models\Socio;
+use App\Models\TipoPersona;
+use App\Models\TipoPersoneria;
+use App\Models\TipoIdentificacion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class SocioController extends Controller
 {
@@ -21,6 +25,10 @@ class SocioController extends Controller
         $provincias = Provincia::where('id_pais', 57)->pluck('nombre', 'id'); // Provincias de Ecuador
         $cantones = Canton::where('id_pais', 57)->where('id_provincia', 2)->pluck('nombre', 'id'); // Provincias de Ecuador
         $parroquias = Parroquia::where('id_pais', 57)->where('id_provincia', 2)->where('id_canton', 2)->pluck('nombre', 'id'); // Provincias de Ecuador
+
+        $tiposPersona = TipoPersona::pluck('descripcion', 'id'); // Obtenemos los tipos de persona
+        $tiposPersoneria = TipoPersoneria::pluck('descripcion', 'id'); // Obtenemos los tipos de personeria
+        $tiposIdentificacion = TipoIdentificacion::pluck('descripcion', 'id'); // Obtenemos los tipos de identificacion
 
         $provinciaDefault = Provincia::find(1); // Obtenemos la provincia con ID = 1
         if ($provinciaDefault) {
@@ -37,60 +45,86 @@ class SocioController extends Controller
             $parroquias->put($parroquiaDefault->id, $parroquiaDefault->nombre); // Añadimos al listado
         }
 
-        return view('administrador.maestro_socios', compact('regimenes', 'paises', 'provincias', 'cantones', 'parroquias'));
+
+        return view('administrador.maestro_socios', compact('regimenes', 'paises', 'provincias', 'cantones', 'parroquias', 'tiposPersona', 'tiposPersoneria', 'tiposIdentificacion'));
     }
 
     public function obtener_listado_socios(Request $request)
     {
-        $columns = [
-            0 => 'socios.id',
-            1 => 'acciones',
-        ];
-
-        $query = Socio::select('fecha_ingreso', 'tipo_persona', 'tipo_personeria', 'razon_social', 'identificacion', 'tipo_identificacion')->where('estado', 1)->orderBy('razon_social')->get();
-
-        //Busqueda
-        if ($search = $request->input('search.value')) {
-            $query->where(function ($query) use ($search) {
-                $query->where('razon_social', 'LIKE', "%{$search}%");
-                //faltan mas
-            });
-        }
-        $totalFiltered = $query->count();
-
-        // Orden
-        $orderColumnIndex = $request->input('order.0.column', 0); // Por defecto, columna 0
-        $orderDir = $request->input('order.0.dir', 'asc'); // Por defecto, orden ascendente
-
-        if (isset($columns[$orderColumnIndex])) {
-            $orderColumn = $columns[$orderColumnIndex];
-            $query->orderBy($orderColumn, $orderDir);
-        }
-
-        // Paginación
-        $start = $request->input('start') ?? 0;
-        $length = $request->input('length') ?? 10;
-        $query->skip($start)->take($length);
-
-        $socios = $query->get();
-        $data = $socios->map(function ($socio) {
-            $boton = "";
-
-            return [
-                ...$socio,
-                'btn' => '<button class="btn btn-primary mb-3 open-modal" data-id="' . $socio->id . '">Modificar</button>' .
-                    '&nbsp;&nbsp;&nbsp;<button class="btn btn-warning mb-3 delete-camara" data-id="' . $socio->id . '">Eliminar</button>' .
-                    '&nbsp;&nbsp;&nbsp;'
+        try {
+            $columns = [
+                0 => 'socios.id',
+                1 => 'razon_social',
             ];
-        });
 
-        $response = [
-            "draw" => intval($request->input('draw')),
-            "recordsTotal" => DB::table('socios')->count(),
-            "recordsFiltered" => $totalFiltered,
-            "data" => $data
-        ];
+            // Construcción de la consulta inicial
+            $query = Socio::with('tipo_identificacion', 'tipo_persona', 'tipo_personeria')
+                ->where('estado', 1);
 
-        return response()->json($response);
+            // Búsqueda
+            if ($search = $request->input('search.value')) {
+                $query->where(function ($q) use ($search) {
+                    $q->where('razon_social', 'LIKE', "%{$search}%")
+                        ->orWhere('tipo_persona.descripcion', 'LIKE', "%{$search}%")
+                        ->orWhere('tipo_identificacion.descripcion', 'LIKE', "%{$search}%");
+                });
+            }
+
+            // Conteo para recordsFiltered
+            $totalFiltered = $query->count();
+
+            // Orden
+            $orderColumnIndex = $request->input('order.0.column', 0); // Por defecto, columna 0
+            $orderDir = $request->input('order.0.dir', 'asc'); // Por defecto, orden ascendente
+
+            if (isset($columns[$orderColumnIndex])) {
+                $orderColumn = $columns[$orderColumnIndex];
+                $query->orderBy($orderColumn, $orderDir);
+            }
+
+            // Paginación
+            $start = $request->input('start', 0);
+            $length = $request->input('length', 10);
+            $query->skip($start)->take($length);
+
+            // Obtener los datos
+            $socios = $query->get();
+            Log::info($socios);
+            // Mapear los datos para la respuesta
+            $data = $socios->map(function ($socio) {
+                $tipo_persona = $socio->tipo_persona->descripcion ?? '';
+                $tipo_personeria = $socio->tipo_personeria->descripcion ?? '';
+                return [
+                    'fecha_ingreso' => $socio->fecha_ingreso,
+                    'razon_social' => $socio->razon_social,
+                    'persona_data' => $tipo_persona . ' - ' . $tipo_personeria,
+                    'identificacion' => $socio->identificacion,
+                    //'direccion' => $socio->direccion,
+                    'tipo_identificacion' => $socio->tipo_identificacion->descripcion ?? '',
+                    'btn' => '<div class="d-flex justify-content-center align-items-center flex-wrap gap-2"><button class="btn btn-primary mb-1 open-modal flex-grow-1 flex-shrink-1" style="min-width: 100px;" data-id="' . $socio->id . '">Modificar</button>' .
+                        '<button class="btn btn-warning mb-1 delete-socio flex-grow-1 flex-shrink-1" style="min-width: 100px;" data-id="' . $socio->id . '">Eliminar</button></div>'
+                ];
+            });
+
+            // Preparar la respuesta
+            $response = [
+                "draw" => intval($request->input('draw')),
+                "recordsTotal" => Socio::where('estado', 1)->count(),
+                "recordsFiltered" => $totalFiltered,
+                "data" => $data,
+            ];
+
+            return response()->json($response);
+        } catch (\Throwable $th) {
+            Log::error($th);
+
+            return response()->json(
+                [
+                    'error' => $th->getMessage(),
+                    'message' => 'Error al obtener los socios',
+                ],
+                500,
+            );
+        }
     }
 }
